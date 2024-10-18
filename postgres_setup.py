eebdf725ebe1aa01915c7cb28f55f61d5186d96a
diogo-dantas <diogo.dantas@live.com>
2024-10-18 08:25:50 -0300

    Criada a função para log de registros.

--- postgres_setup.py
+++ postgres_setup.py
@@ -1,5 +1,9 @@
 import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import pandas as pd
+from typing import Optional, Dict, List, Union
+import logging
 from datetime import datetime
 import os
 
@@ -14,7 +18,7 @@ class PostgresConnector:
         port: str = '5432'
     ):
 
-# Inicializa o conector com PostgreSQL
+""" Inicializa o conector com PostgreSQL """
 
     self.credentials = {
             'dbname': dbname,
@@ -24,4 +28,22 @@ class PostgresConnector:
             'port': port
         }
 
+self.setup_logging()
 
+"""Configura o logging para registro de operações no diretório log_dir (criado)"""
+
+ def setup_logging(self):
+        log_dir = 'logs'
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+            
+        log_file = os.path.join(
+            log_dir, 
+            f'postgres_operations_{datetime.now().strftime("%Y%m%d")}.log'
+        )
+        
+        logging.basicConfig(
+            filename=log_file,
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
